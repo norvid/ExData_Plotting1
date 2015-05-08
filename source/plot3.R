4efdd273693c464d50f444cbe7e66b17b1ba540f
norvid <lintomny@163.com>
2015-05-08 22:39:32 +0800

    Create plot3.R

---
+++ source/plot3.R
@@ -0,0 +1,34 @@
+#  filename : plot3.R
+#  create by: norvid, 2015-05-08
+#  memo     : exdata week 1 homework
+
+df<- read.table("household_power_consumption.txt", sep=";", header=TRUE, stringsAsFactor=FALSE)
+# get sub dataset that Date in 01/02/2007 ~ 02/02/2007.
+sds <- df[which(df$Date == '1/2/2007' | df$Date == '2/2/2007'), ]
+remove(df)
+
+# clean data
+sds$Global_active_power[sds$Global_active_power == '?'] <- NA
+sds$Global_reactive_power[sds$Global_reactive_power == '?'] <- NA
+sds$Voltage[sds$Voltage == '?'] <- NA
+sds$Global_intensity[sds$Global_intensity == '?'] <- NA
+sds$Sub_metering_1[sds$Sub_metering_1 == '?'] <- NA
+sds$Sub_metering_2[sds$Sub_metering_2 == '?'] <- NA
+sds$Sub_metering_3[sds$Sub_metering_3 == '?'] <- NA
+
+sds$DateTime              <- as.POSIXct(paste(sds$Date, sds$Time, sep = " "), format = "%d/%m/%Y %H:%M:%S")
+sds$Global_active_power   <- as.numeric(sds$Global_active_power)
+sds$Global_reactive_power <- as.numeric(sds$Global_reactive_power)
+sds$Voltage               <- as.numeric(sds$Voltage)
+sds$Global_intensity      <- as.numeric(sds$Global_intensity)
+sds$Sub_metering_1        <- as.numeric(sds$Sub_metering_1)
+sds$Sub_metering_2        <- as.numeric(sds$Sub_metering_2)
+sds$Sub_metering_3        <- as.numeric(sds$Sub_metering_3)
+
+# Plot3: Energy sub metering
+png(filename = "plot3.png", width = 480, height = 480, units = "px")
+plot(sds$DateTime, sds$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(sds$DateTime, sds$Sub_metering_2, col = "red")
+lines(sds$DateTime, sds$Sub_metering_3, col = "blue")
+legend("topright", lty=1, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"))
+dev.off()
